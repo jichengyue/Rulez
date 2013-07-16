feea7607b95eef506e60035ceb636ef16e79066e
StefanoOrdine <stefano.ordine@gmail.com>
2013-07-16 15:53:22 +0200

    removed test class

--- lib/rulez/engine.rb
+++ lib/rulez/engine.rb
@@ -3,13 +3,5 @@ module Rulez
     isolate_namespace Rulez
   end
 
-  class MiaClasse
-    def self.saluta
-      p "Ciao!!!"
-    end
-    @@var = 4
-    def self.stampavar
-      p "#{@@var}"
-    end
-  end
+  
 end

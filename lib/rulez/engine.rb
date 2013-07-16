9c8cc5fa2d002ac29d638547da5fd3617a22613e
StefanoOrdine <stefano.ordine@gmail.com>
2013-07-16 15:51:48 +0200

    parser added
    included parser folder

--- lib/rulez/engine.rb
+++ lib/rulez/engine.rb
@@ -2,4 +2,14 @@ module Rulez
   class Engine < ::Rails::Engine
     isolate_namespace Rulez
   end
+
+  class MiaClasse
+    def self.saluta
+      p "Ciao!!!"
+    end
+    @@var = 4
+    def self.stampavar
+      p "#{@@var}"
+    end
+  end
 end
---
+++ lib/rulez/parser/rulezparser.rb
@@ -0,0 +1,34 @@
+require 'treetop'
+require File.join(File.expand_path(File.dirname(__FILE__)),'rulezgrammar.rb')
+
+
+class RulezParser
+  # retrive current base path
+  @@base_path = File.expand_path(File.dirname(__FILE__))
+  # create the parser class from the grammar defined in rulezgrammar.treetop
+  #Treetop.load(File.join(@@base_path,'rulezgrammar.treetop'))
+  # instantiate the parser
+  @@parser = RulezGrammarParser.new
+
+  # 
+  # parse top down the incoming input data
+  # @param  data  string that describe the rule to be parsed
+  # 
+  # @return Treetop::Runtime::SyntaxNode 
+  def self.parse(data)
+    tree = @@parser.parse(data)
+
+    # aggiungere qui un controllo di errore sul tree
+    # se tree.nil? allora il parsing non ha avuto successo
+
+    return tree
+  end
+
+  def self.TypeDate(d)
+    Date.strptime(d,'%d//%m//%Y')
+  end
+
+  def self.TypeDateTime(dt)
+    DateTime.strptime(dt,'%d//%m//%Y#%H:%M:%S')
+  end
+end

9ba87c16e5cf8d5b19900a7df9417ebcefe8afe8
Chosko <ruben.caliandro@gmail.com>
2013-07-18 16:14:12 +0200

    converted rule from string to text

--- app/validators/rulez/syntax_validator.rb
+++ app/validators/rulez/syntax_validator.rb
@@ -1,7 +1,6 @@
 module Rulez
   class SyntaxValidator < ActiveModel::EachValidator
     def validate_each(object, attribute, value)
-      debugger
       if !value || !Parser.parse(value.gsub(/\s+/, ''))
         object.errors[attribute] << (options[:message] || "is not a valid expression.")
       end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130718130318) do
+ActiveRecord::Schema.define(:version => 20130718135643) do
 
   create_table "rulez_contexts", :force => true do |t|
     t.string   "name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(:version => 20130718130318) do
   create_table "rulez_rules", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.string   "rule"
+    t.text     "rule"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "context_id"

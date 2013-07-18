45fe0b4181a4c3c78a7f6521bb5ec1d9d7d5ea0d
Chosko <ruben.caliandro@gmail.com>
2013-07-18 15:09:32 +0200

    models annotated

--- app/models/rulez/rule.rb
+++ app/models/rulez/rule.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: rulez_rules
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  rule        :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  context_id  :integer
+#
+
 module Rulez
   class Rule < ActiveRecord::Base
     attr_accessible :description, :name, :rule
--- app/models/rulez/symbol.rb
+++ app/models/rulez/symbol.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: rulez_symbols
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 module Rulez
   class Symbol < ActiveRecord::Base
     attr_accessible :description, :name

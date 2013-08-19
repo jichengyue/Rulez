cc12264d2bf09c555230e9a48f1e013cf8b87a4a
Stefano Nada <stefano.nada@gmail.com>
2013-08-19 17:02:39 +0200

    fix doc

--- test/dummy/app/models/restaurant.rb
+++ test/dummy/app/models/restaurant.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  city       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Restaurant < ActiveRecord::Base
   attr_accessible :city, :name
 end
--- test/unit/rulez/symbol_test.rb
+++ test/unit/rulez/symbol_test.rb
@@ -5,6 +5,7 @@
 #  id          :integer          not null, primary key
 #  name        :string(255)
 #  description :text
+#  model       :string(255)
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #

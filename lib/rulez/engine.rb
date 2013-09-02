e26a0d83d43ce7a214ea03e545fbaa701a899199
Chosko <ruben.caliandro@gmail.com>
2013-09-02 17:42:53 +0200

    removed factory_girl

--- lib/rulez/engine.rb
+++ lib/rulez/engine.rb
@@ -18,7 +18,6 @@ module Rulez
     #Rspec configuration
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
       g.assets false
       g.helper false
     end
